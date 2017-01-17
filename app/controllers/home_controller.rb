947f6a04ddaa234294220096aa938d32655d88d1
nisroeld99 <hellowworld1990@gmail.com>
2017-01-17 15:12:02 +0900

    down

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,6 @@ class HomeController < ApplicationController
   def doc
     # send_file Rails.root.join('private', 'Rogelio Alvarado Vilchis.docx'), :type=>"application/doc", :x_sendfile=>true
     data = open("https://s3.ap-northeast-2.amazonaws.com/nisrodb/SPRINT_APPLY.docx") 
-  send_data data.read, filename: "hi.doc", type: "application/doc", stream: 'true', buffer_size: '4096'    
+  send_data data.read, filename: "hi.doc", type: "application/doc", disposition: 'inline', stream: 'true', buffer_size: '4096'    
   end
 end
