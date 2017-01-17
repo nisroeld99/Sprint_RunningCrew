023f87d7f9b34e25c0523e8b6b21041557e867f5
nisroeld99 <hellowworld1990@gmail.com>
2017-01-17 11:02:39 +0900

    acc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def doc
+    send_file Rails.root.join('private', 'Rogelio Alvarado Vilchis.docx'), :type=>"application/doc", :x_sendfile=>true
+  end
 end
